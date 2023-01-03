3360b20303435b8bd7f2aae85eb9a91e39ae8f54
rmunozfernandez <rmunoz.it@gmail.com>
2023-01-02 21:12:21 -0300

    Leer imputs desde la consola

---
+++ calculadora.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	operacion := scanner.Text()
+	valores := strings.Split(operacion, "+")
+	fmt.Println(valores)
+	fmt.Println(valores[0] + valores[1])
+	valor1, _ := strconv.Atoi(valores[0])
+	valor2, _ := strconv.Atoi(valores[1])
+	fmt.Println(valor1 + valor2)
+}
