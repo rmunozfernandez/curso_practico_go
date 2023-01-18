82540bf6e6cee829269c610a834a0df019d50126
rmunozfernandez <rmunoz.it@gmail.com>
2023-01-18 19:56:24 -0300

    Structs para Lista de Tareas - Pendiente

---
+++ 07_structs_para_Lista_de_Tareas/tareas.go
@@ -0,0 +1,12 @@
+package main
+
+/* Requerimientos del programa
+- crear un struct task
+- crear las propiedades nombre string, descripción string y completado bool
+- instanciarlo
+- imprimir la informacion de la instancia con su clave y valor
+*/
+
+func main() {
+
+}
